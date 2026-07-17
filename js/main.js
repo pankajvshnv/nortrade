ae7960faeecaebf1c00eb7d42ea2e47cfca21219
pankajvshnv <nickvshnv@gmail.com>
2026-07-17 12:42:03 +0530

    feat: allow scroll reveal animations to trigger repeatedly on scroll up and down

--- js/main.js
+++ js/main.js
@@ -286,7 +286,9 @@ function initScrollReveal() {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
         entry.target.classList.add('in-view');
-        observer.unobserve(entry.target);
+      } else {
+        // Remove class when out of view so it animates again when scrolling back
+        entry.target.classList.remove('in-view');
       }
     });
   }, { threshold: 0.1, rootMargin: '0px 0px -50px 0px' });
